34dbf78911302f629b6245b652b9cc3a9b55b0c9
LuisM <muislarques@gmail.com>
2025-08-02 16:32:51 +0100

    new: default setup to connect mvc app to database (item, controller, view, appsettings with connectionstring to bd)

---
+++ SuperMarketComparison/Controllers/ItemsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SuperMarketComparison.Models;
+
+namespace SuperMarketComparison.Controllers
+{
+    public class ItemsController : Controller
+    {
+        public IActionResult Overview()
+        {
+            var item = new Item() { Name = "papel higienico" };
+            return View(item);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return Content("id= " + id);
+        }
+    }
+}
---
+++ SuperMarketComparison/Data/SMCContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperMarketComparison.Models;
+
+namespace SuperMarketComparison.Data
+{
+    public class SMCContext : DbContext
+    {
+        public SMCContext(DbContextOptions<SMCContext> options) : base(options) { }
+        public DbSet<Item> Items { get; set; }
+    }
+}
