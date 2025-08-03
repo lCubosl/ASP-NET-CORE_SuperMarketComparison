4f75614db2ebacec99e552f017f91485077ba3ed
LuisM <muislarques@gmail.com>
2025-08-04 00:44:38 +0100

    new: store (model:store, view:create, controller:storescontroller(create view))

---
+++ SuperMarketComparison/Controllers/StoresController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SuperMarketComparison.Controllers
+{
+    public class StoresController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
