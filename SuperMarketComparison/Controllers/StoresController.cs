e4675d7902a7e0476ccb95f35a7dfc766b183559
LuisM <muislarques@gmail.com>
2025-08-04 12:34:25 +0100

    new: view dleete updates: controller paths for views and methods; updated edit and index views

--- SuperMarketComparison/Controllers/StoresController.cs
+++ SuperMarketComparison/Controllers/StoresController.cs
@@ -13,6 +13,7 @@ namespace SuperMarketComparison.Controllers
             _context = context;
         }
 
+        // INDEX
         // view INDEX
         public async Task<IActionResult> Index()
         {
@@ -20,6 +21,7 @@ namespace SuperMarketComparison.Controllers
             return View(store);
         }
 
+        // CREATE
         // view CREATE
         public IActionResult Create()
         {
@@ -36,6 +38,46 @@ namespace SuperMarketComparison.Controllers
                 return RedirectToAction("Index", "Items");
             }
             return View(store);
+        }
+
+        // EDIT
+        // view Edit
+        public async Task<IActionResult> Edit(int id)
+        {
+            var store = await _context.Stores.FirstOrDefaultAsync(x => x.Id == id);
+            return View(store);
+        }
+        // update db UPDATE store
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id, Name, Location")] Store store)
+        {
+            if ( ModelState.IsValid )
+            {
+                _context.Stores.Update(store);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(store);
+        }
+
+        // DELETE
+        // view DELETE
+        public async Task<IActionResult> Delete(int id)
+        {
+            var store = await _context.Stores.FirstOrDefaultAsync(x => x.Id == id);
+            return View(store);
+        }
+        // update db DELETE store
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var store = await _context.Stores.FindAsync(id);
+            if (store != null)
+            {
+                _context.Stores.Remove(store);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("Index");
         }
     }
 }
