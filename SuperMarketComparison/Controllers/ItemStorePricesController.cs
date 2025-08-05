128dc531331e6b031af4072b0cb2e85b30ba38c0
LuisM <muislarques@gmail.com>
2025-08-05 16:50:31 +0100

    updates: trying to debug why create doesnt work on itemstoreprices

--- SuperMarketComparison/Controllers/ItemStorePricesController.cs
+++ SuperMarketComparison/Controllers/ItemStorePricesController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using SuperMarketComparison.Data;
 using SuperMarketComparison.Models;
 using System.Threading.Tasks;
@@ -13,12 +15,19 @@ namespace SuperMarketComparison.Controllers
             _context = context;
         }
 
+        // CREATE
+        // view CREATE /itemstoreprices/create/{id}
         public async Task<IActionResult> Create(int id)
         {
+            Console.WriteLine("VIEW for itemstoreprice triggered");
+            
             var item = await _context.Items.FindAsync(id);
             
             if (item == null)
                 return NotFound();
+
+            var stores = await _context.Stores.ToListAsync();
+            ViewBag.StoreList = new SelectList(stores, "Id", "Name");
 
             var model = new ItemStorePrice
             {
@@ -26,6 +35,37 @@ namespace SuperMarketComparison.Controllers
                 Item = item,
                 LastUpdate = DateTime.Today
             };
+
+            return View(model);
+        }
+
+        // update db CREATE new itemstoreprice
+        [HttpPost]
+        public async Task<IActionResult> Create(int id, [Bind("ItemId, StoreId, Price, LastUpdate")] ItemStorePrice model)
+        {
+            Console.WriteLine("POST for itemstoreprice triggered");
+
+            if (ModelState.IsValid)
+            {
+                _context.ItemStorePrices.Add(model);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Details", "Items", new { id = model.ItemId });
+            }
+
+            // log for validation errors 
+            foreach(var state in ModelState)
+            {
+                foreach (var error in state.Value.Errors)
+                    Console.WriteLine($"Key: {state.Key} Error: {error.ErrorMessage}");
+            }
+
+            // after submit, item will be set to null and page will throw an error. this is a simple
+            // fix to repopulate item for display
+            model.Item = await _context.Items.FindAsync(model.ItemId);
+
+            var stores = await _context.Stores.ToListAsync();
+            ViewBag.StoreList = new SelectList(stores, "Id", "Name");
 
             return View(model);
         }
--- SuperMarketComparison/Models/ItemStorePrice.cs
+++ SuperMarketComparison/Models/ItemStorePrice.cs
@@ -8,17 +8,21 @@ namespace SuperMarketComparison.Models
 
         // FOREIGN KEYS
         // foreign key ItemId from Item
+        [Required]
         public int ItemId { get; set; }
         public Item Item { get; set; }
 
         // foreign key StoreId from Store
+        [Required]
         public int StoreId { get; set; }
         public Store Store { get; set; }
 
         // Adicional fields PRICE, DATE
+        [Required]
         [DataType(DataType.Currency)]
         public decimal Price { get; set; }
 
+        [Required]
         [DataType(DataType.Date)]
         public DateTime LastUpdate { get; set; }
     }
