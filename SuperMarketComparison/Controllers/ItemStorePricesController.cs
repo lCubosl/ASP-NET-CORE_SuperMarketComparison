ab382e76c09a9eb5dcb13a707135fefc81890868
LuisM <muislarques@gmail.com>
2025-08-06 00:58:38 +0100

    new: view for /itemstoreprices/edit/id (edit) updates: routes, buttons, itemstorepricescontroller to pass the itemstoreprice model into edit view

--- SuperMarketComparison/Controllers/ItemStorePricesController.cs
+++ SuperMarketComparison/Controllers/ItemStorePricesController.cs
@@ -43,10 +43,12 @@ namespace SuperMarketComparison.Controllers
         [HttpPost]
         public async Task<IActionResult> Create([Bind("ItemId, StoreId, Price, LastUpdate")] ItemStorePrice model)
         {
+            // check if post is at least  getting triggered
             Console.WriteLine("POST for itemstoreprice triggered");
 
             if (ModelState.IsValid)
             {
+                // checking if model isvalid
                 Console.WriteLine("ModelState IS VALID");
                 _context.ItemStorePrices.Add(model);
                 await _context.SaveChangesAsync();
@@ -54,21 +56,34 @@ namespace SuperMarketComparison.Controllers
                 return RedirectToAction("Details", "Items", new { id = model.ItemId });
             }
 
-            // log for validation errors 
+            // log for validation errors REMOVE LATER
+            // logs what fields are empty (ItemId, StoreId, Price, etc...)
+            // ex: Key: Item Error: The Item field is required
             foreach (var state in ModelState)
             {
                 foreach (var error in state.Value.Errors)
                     Console.WriteLine($"Key: {state.Key} Error: {error.ErrorMessage}");
             }
 
-            // after submit, item will be set to null and page will throw an error. this is a simple
-            // fix to repopulate item for display
-            model.Item = await _context.Items.FindAsync(model.ItemId);
-
             var stores = await _context.Stores.ToListAsync();
             ViewBag.StoreList = new SelectList(stores, "Id", "Name");
 
             return View(model);
+        }
+
+        // EDIT
+        // view EDIT /itemstoreprices/edit/{id}
+        public async Task<IActionResult> Edit(int id)
+        {
+            var item = await _context.ItemStorePrices
+                .Include(i => i.Item)
+                .Include(i => i.Store)
+                .FirstOrDefaultAsync(i => i.Id == id);
+
+            if (item == null)
+                return NotFound();
+
+            return View(item);
         }
     }
 }
